9115e63d5e30b0a898b0f2bc79e736699c86797c
Вадим Горбатов <75436335+pydev02@users.noreply.github.com>
2022-04-22 17:17:14 +0300

    Create boiling.go

---
+++ Глава 2/boiling.go
@@ -0,0 +1,12 @@
+// boiling выводит температуру кипения воды
+package main
+
+import "fmt"
+
+const boilingF = 212.0
+
+func main() {
+	var f = boilingF
+	var c = (f - 32) * 5 / 9
+	fmt.Printf("Температура кипения = %g°F или %g°С\n", f, c)
+}
